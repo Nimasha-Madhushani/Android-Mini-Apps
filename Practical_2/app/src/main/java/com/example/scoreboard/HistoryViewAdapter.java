83d2d082f8e69d58e04f8d434a56b0d6c2862647
Nimasha-Madhushani <nimashamadhushani19@gmail.com>
2022-09-22 07:37:35 +0530

    initiate the adapter and create viewholder

---
+++ Practical_2/app/src/main/java/com/example/scoreboard/HistoryViewAdapter.java
@@ -0,0 +1,52 @@
+package com.example.scoreboard;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class HistoryViewAdapter extends RecyclerView.Adapter<HistoryViewAdapter.ViewHolder> {
+    private List<String> mData;
+    private LayoutInflater mInflater;
+
+    //data is passed in to the constructor
+    HistoryViewAdapter(Context context, List<String> data) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mData = data;
+    }
+
+    //stores and recycle view as they are scrolled off screen
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView myTextView;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            myTextView = itemView.findViewById(R.id.tv_score_history);
+        }
+
+
+    }
+
+    //Inflates the row layout from xml been needed
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.history_cell, parent, false) {
+            return new ViewHolder(view);
+        }
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+//total no:of rows
+}
